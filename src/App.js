826a0f48f30e7ef2d23d00e484cadcc070c5a49e
nihisd-max <nithishdnr3@gmail.com>
2020-12-29 17:25:13 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Timer extends React.Component {
+  state = {
+    isActive: false,
+    secondsElapsed: 0 / 1000 //time in seconds
+  };
+
+  getHours() {
+    return ("0" + Math.floor(this.state.secondsElapsed / 3600)).slice(-2);
+  }
+
+  getMinutes() {
+    return ("0" + Math.floor((this.state.secondsElapsed % 3600) / 60)).slice(
+      -2
+    );
+  }
+
+  getSeconds() {
+    return ("0" + (this.state.secondsElapsed % 60)).slice(-2);
+  }
+
+  startTime = () => {
+    this.setState({ isActive: true });
+
+    this.countdown = setInterval(() => {
+      this.setState(({ secondsElapsed }) => ({
+        secondsElapsed: secondsElapsed + 1
+      }));
+    }, 1000);
+  };
+
+  resetTime = () => {
+    clearInterval(this.countdown);
+    this.setState({
+      secondsElapsed: 0 / 1000,
+      isActive: false
+    });
+  };
+
+  pauseTime = () => {
+    clearInterval(this.countdown);
+    this.setState({ isActive: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Timer-container">
+          <span className="Bloc-timer"> {this.getHours()}</span>
+          <span className="Bloc-timer"> :{this.getMinutes()}</span>
+          <span className="Bloc-timer"> :{this.getSeconds()}</span>
+        </div>
+        <div>
+          <button
+            className="Button StartButton"
+            onClick={this.state.isActive ? this.pauseTime : this.startTime}
+          >
+            Start/Pause
+          </button>
+          <button className="Button PauseButton" onClick={this.resetTime}>
+            Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
